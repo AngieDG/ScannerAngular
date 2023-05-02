e83fd8c8b7809845a09499a8871b72db26a55eae
Angelica De La Cruz <adelacruz.future@gmail.com>
2023-05-02 00:42:21 -0600

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  ScannerQRCodeConfig,
+  ScannerQRCodeResult,
+  NgxScannerQrcodeService,
+  NgxScannerQrcodeComponent,
+  ScannerQRCodeSelectedFiles,
+} from 'ngx-scanner-qrcode';
+import { delay } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ScannCode';
+
+export class AppComponent implements AfterViewInit {
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#front_and_back_camera
+  public config: ScannerQRCodeConfig = {
+    // fps: 1000,
+    vibrate: 400,
+    // isBeep: true,
+    // decode: 'macintosh',
+    deviceActive: 1, // camera front: deviceActive=0  // back camera: deviceActive=1
+    constraints: {
+      facingMode: "environment", // 'user' (front camera), and 'environment' (back camera).
+      audio: false,
+      video: {
+        width: window.innerWidth // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+      }
+    }
+  };
+
+  public qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
+  public qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
+
+  @ViewChild('action')
+  action!: NgxScannerQrcodeComponent;
+
+  constructor(private qrcode: NgxScannerQrcodeService) { }
+
+  ngAfterViewInit(): void {
+    this.action.isReady.pipe(delay(3000)).subscribe(() => {
+      this.action.start();
+    });
+  }
+
+  public onEvent(e: ScannerQRCodeResult[]): void {
+    console.log(e);
+  }
+
+  public handle(action: NgxScannerQrcodeComponent, fn: string): void {
+   // action[fn]().subscribe(console.log, alert);
+  }
+
+  public onSelects(files: any): void {
+    this.qrcode.loadFiles(files).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      this.qrCodeResult = res;
+    });
+  }
+
+  public onSelects2(files: any): void {
+    this.qrcode.loadFilesToScan(files, this.config).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      console.log(res);
+      this.qrCodeResult2 = res;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxScannerQrcodeModule,
+    FormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
